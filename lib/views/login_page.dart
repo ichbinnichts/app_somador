a91d70ed98428d9c4e9487ff8ca64b35f0e802c4
Nathan Faria <nathanfaria607@gmail.com>
2024-06-18 07:54:37 -0300

    feat: add login page

---
+++ lib/views/login_page.dart
@@ -0,0 +1,18 @@
+import 'package:app_somador/components/main_app_bar_component.dart';
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: appBar(),
+    );
+  }
+}
