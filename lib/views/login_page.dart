7be3c4448981e46161ea75a093196f68406479ad
Nathan Faria <nathanfaria607@gmail.com>
2024-06-18 08:13:21 -0300

    feat: implement login page

--- lib/views/login_page.dart
+++ lib/views/login_page.dart
@@ -1,4 +1,6 @@
+import 'package:app_somador/components/form_text_field_component.dart';
 import 'package:app_somador/components/main_app_bar_component.dart';
+import 'package:app_somador/components/main_button_component.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatefulWidget {
@@ -13,6 +15,31 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: appBar(),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Login',
+              style: TextStyle(
+                fontSize: 24,
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            FormTextField(label: 'Email', hint: 'example@email.com'),
+            FormTextField(label: 'Senha', hint: 'Senha'),
+            const SizedBox(
+              height: 10,
+            ),
+            MainButton(
+              text: 'Entrar',
+              onPressed: () {},
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
