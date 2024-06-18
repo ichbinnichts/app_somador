8ee5f80891ad212a74cc22ff5f9750e3420d88a1
Nathan Faria <nathanfaria607@gmail.com>
2024-06-18 07:54:11 -0300

    feat: add main app bar component

---
+++ lib/components/main_app_bar_component.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+AppBar appBar() {
+  return AppBar(
+    title: const Text('Somador'),
+    leading: const Icon(Icons.calculate_rounded),
+    backgroundColor: Colors.blue,
+    foregroundColor: Colors.white,
+  );
+}
